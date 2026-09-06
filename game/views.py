1c9c3251e05fb43d16a0d6f4fd649ee271ed9a7e
itoncek <itoncek@itoncek.space>
2026-09-06 13:02:45 +0200

    Update game/views.py

--- game/views.py
+++ game/views.py
@@ -324,7 +324,7 @@ def process_zone_interaction(team, zone):
     return JsonResponse({'status': 'ok'})
 
 QR_SECRET = "17fm!5nj(gzjv)uvtffhijj9ojgrderfh90kbte2(5f3q8c7=az_17rs9@2t1f"
-QR_BASE_URL = "https://game.maftyzak.cz/c/"
+QR_BASE_URL = "https://game.matfyzak.cz/c/"
 
 def make_signature(zone_id: str) -> str:
     return hashlib.sha256(f"{zone_id}-{QR_SECRET}".encode()).hexdigest()[:3].upper()
